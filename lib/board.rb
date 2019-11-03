abd693201e33cdbd3b2194332ff3bc38456aad4d
lior Shine <shinelior@gmail.com>
2019-11-03 04:06:00 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -7,15 +7,8 @@ def board
 
 board=[["","",""],["","",""],["","",""]]
 
-board[0]="h"
-board[1]="e"
-board[2]="l"
-board[3]="o"
-board[4]="w"
-board[5]="w"
-board[6]="o"
-board[7]="l"
-board[8]="t"
+
+
  return board
 end
 
