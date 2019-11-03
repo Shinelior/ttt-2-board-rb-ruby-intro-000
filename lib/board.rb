04a237bb6db1c4166b98874de00ba62c90fa4941
lior Shine <shinelior@gmail.com>
2019-11-03 03:51:39 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,22 @@
 # Define the variable board below.
+def board()
+
+
+
+
+
 board=[["","",""],["","",""],["","",""]]
+
+board[0]="h"
+board[1]="e"
+board[2]="l"
+board[3]="o"
+board[4]="w"
+board[5]="w"
+board[6]="o"
+board[7]="l"
+board[8]="t"
+
+end
+
+
